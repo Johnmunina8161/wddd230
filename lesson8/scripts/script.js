62fef67c8a620da1eaad014106bd33832001c9b0
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2022-10-31 09:05:06 +0200

    tablebuild

---
+++ lesson8/scripts/script.js
@@ -0,0 +1,26 @@
+WebFont.load({
+    google: {
+        families: [
+            'Comfortaa',
+            'Exo 2'
+        ]
+    }
+})
+
+document.getElementsByClassName('last-updated')[0].innerHTML = "Last Updated: "+document.lastModified
+
+function toggleMenu() {
+    var temp = document.getElementsByClassName("toggler")[0]
+    temp.classList.toggle("hide")
+    if (temp.classList[1]=="hide") {
+        document.querySelector(".toggler > a").innerHTML = "&#9776; Menu"
+    }
+    else {
+        document.querySelector(".toggler > a").innerHTML = "&#10006; Close"
+    }
+}
+
+
+adjustLabel = (elem) => {
+    document.querySelector("label[for='severity']").innerHTML = elem.value
+}
