24973b085b8a77306c2e44f189722f5076c1f6a3
John Munina <97455093+Johnmunina8@users.noreply.github.com>
2022-11-28 11:23:20 +0200

    Chamber project

---
+++ lesson11/scripts/hamburger.js
@@ -0,0 +1,10 @@
+// Hamburger Menu
+
+let hamburger = document.querySelector('#hamburger');
+let nav = document.querySelector('header');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('responsive');
+
+});
+
